f8bd648094a3b2b51affbbe88b50c20bc25cfb96
zubowaantonina <zubowaantonina@yandex.ru>
2025-10-21 18:41:26 +0300

    Урок 3. Слайдер, адаптив и github pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 new WOW().init();
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  spaceBetween: 100,
+   pagination: {
+    el: '.project-pagination',
+    bulletClass:'project-bullet',
+    bulletActiveClass:'project-bullet-activ',
+    clickable:true
+  },
+});
